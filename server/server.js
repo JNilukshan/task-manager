36a7bb284b7bc85218306c02f3942df5b7a7021b
S H M J N Sooriyahetti <janithnilukshan@gmail.com>
2025-08-05 12:07:42 +0530

    deploy_3

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/taskmanager';
+const MONGODB_URI = process.env.MONGODB_URI 
 
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('✅ Connected to MongoDB'))
